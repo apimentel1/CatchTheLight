66b2f9c8999571442ab42709537d310e8543bd89
Alec Pimentel <apimentel1@umassd.edu>
2019-10-25 22:12:31 -0400

    Started to update code for current PCB layout

--- Catch_The_Light/main.c
+++ Catch_The_Light/main.c
@@ -14,24 +14,28 @@ void Cycle_lights_Rev();
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-int Flashreg = 7;
+//uint16_t Ledtab [] = {0x08, 0x04, 0x02, 0x01, 0x20, 0x16, 0x08, 0x04, 0x16, 0x08, 0x04, 0x02, 0x01, 0x80, 0x40, 0x20};
+uint8_t Ledtab [] = {3, 2, 1, 0, 5, 4, 3, 2, 4, 3, 2, 1, 0, 7, 6, 5};
+
+int Flashreg = 0;
 int Currentport = 1;
 
 int main(void)
 {
 	DDRD &= 0xFF;
 	DDRB &= 0xFF;
+	DDRC &= 0xFF;
 	
 	
 	PORTB &= 0;
 	PORTD &= 0; //Turn off leds
 	
-	All_On();
+	//All_On();
 	
     /* Replace with your application code */
     while (1) 
     {
-		Cycle_lights_Rev();
+		Cycle_lights();
     }
 }
 
@@ -39,7 +43,29 @@ void Cycle_lights()
 {
 	PORTD &= 0x0; //turn off all led
 	PORTB &= 0x0;
+	PORTC &= 0x0;
+	//int Pin = Ledtab[Flashreg];
+	if(Flashreg < 4 || Flashreg > 12)
+	{
+		//PORTD = Ledtab[Flashreg];
+		PORTD |= (1<< Ledtab[Flashreg]);
+	}
+	else if(Flashreg < 8)
+	{
+		//PORTC = Ledtab[Flashreg];
+		PORTC |= (1<< Ledtab[Flashreg]);
+	}
+	else
+	{
+		//PORTD = Ledtab[Flashreg];
+		PORTB |= (1<< Ledtab[Flashreg]);
+	}
 	
+	Flashreg = (Flashreg +1) & 15;
+	
+	_delay_ms(50);
+	
+	/*
 	if(Currentport)
 	{
 		PORTD |= (1<< Flashreg);
@@ -54,7 +80,7 @@ void Cycle_lights()
 	{
 		Currentport = Currentport^1; //toggle Currentport between 1 and 0
 	}	
-	_delay_ms(50);
+	_delay_ms(50);*/
 }
 
 void Cycle_lights_Rev()
