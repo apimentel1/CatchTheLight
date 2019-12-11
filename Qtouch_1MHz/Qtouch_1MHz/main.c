762db482b2e5126be48cf5dc7ef4e6755baacfbe
Alec Pimentel <apimentel1@umassd.edu>
2019-12-10 21:20:11 -0500

    Added qtouch project

--- Qtouch_1MHz/Qtouch_1MHz/main.c
+++ Qtouch_1MHz/Qtouch_1MHz/main.c
@@ -1,10 +1,10 @@
-/*
- * Catch_The_Light.c
- *
- * Created: 10/18/2019 9:08:03 PM
- * Author : pimen
- */ 
+#include <atmel_start.h>
+#include <touch.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
 
+int is_touched();
+void Did_you_catch_the_light();
 void Cycle_lights();
 void All_On(int Flashtime);
 void FlashPort(int Led);
@@ -12,68 +12,142 @@ void Cycle_lights_Rev();
 void mydelay(volatile int Delay);
 void Light_Direction();
 
-#include <avr/io.h>
-#define F_CPU 1000000UL
-#include <util/delay.h>
-#include <avr/interrupt.h>
-
-//uint16_t Ledtab [] = {0x08, 0x04, 0x02, 0x01, 0x20, 0x16, 0x08, 0x04, 0x16, 0x08, 0x04, 0x02, 0x01, 0x80, 0x40, 0x20};
 //PORTD has Leds 1-4, 14-16
 //PORTB has Leds 9-13
 //PORTC has Leds 5-8
 //-------------Led # 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16
 uint8_t Ledtab [] = {3, 2, 1, 0, 5, 4, 3, 2, 4, 3, 2, 1, 0, 7, 6, 5};
-
-
-int Flashreg = 0;
-#define button 1
-int Direction = 1;
-volatile int Delay_Time = 390; //100ms with 256 pre
+volatile int Flashreg = 0;
+volatile int Direction = 1;
+volatile int Delay_Time = 390;
 int Win_time = 0;
+volatile int Flag = 1;
+
+	
 
 int main(void)
-{
-	DDRD |= 0xFF;
-	DDRB |= 0x7F;
-	DDRC = 0xFF & ~(1<<button) ;
+{	
+	DDRD = 0xFF;
+	DDRB = 0xFF;
+	DDRC = 0xFF;
 	
-	
-	PORTC |= (1<<button); //enable pull-up resistor for PB7
-	
+	PORTB |= 0x0;
 	PORTD |= 0x0; //Turn off leds
-	PORTB |= ~(0x7F);
+	PORTC |= 0x0;
 	
+	atmel_start_init();
+	cpu_irq_enable();
 	
-	//PORTC |= 0x0;
 	TCCR1A = 0x2; // This is in CTC Mode
 	TCCR1B = 0x4; //256 pre
 	TIMSK1 = (1<<OCIE1A);
-	//OCIE1A = Delay_Time;
-	//TIFR1 = (1<<OCIE1A);
-	//TCNT1 = Delay_Time;
 	OCR1A = Delay_Time;
-	PCICR |= (1 << PCIE1);
-	PCMSK1 |= (1 << PCINT9);
 	sei();
 	
-	
-	All_On(5);
-	
-
-    while (1) 
-    {
-		//Light_Direction();
-    }
+	//All_On(5);
+	while( 1)
+	{
+		while (!is_touched())
+			;//PIND = 1<<5;
+		//PORTD &=~(1<<5);
+		
+		Did_you_catch_the_light();
+		
+		//PORTD|=(1<<3);
+		
+		while (is_touched())
+			;//PIND = 1<<2;
+		//PORTD &= ~(1<<2);
+		
+		//PORTD&=(1<<3);*/
+		
+		
+		
+	}
 }
 
+int is_touched()
+{
+	uint8_t key_status1 = 0;
+	
+	touch_process();
+	key_status1 = get_sensor_state(0) & 0x80;
 
+	if ((0u != key_status1))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+void Did_you_catch_the_light()
+{
+	Flag = 0;
+	if(Flashreg == 0)
+	{
+		All_On(5);
+		Direction ^= 1;
+		if(Win_time > 1)
+		{
+			Delay_Time -= 78; //subtract 20ms
+			if(Delay_Time < 1)
+			{
+				Delay_Time = 390; //reset to 100ms
+				Win_time = 0;
+			}
+		}
+		Win_time++;
+	}
+	else
+	{
+		PORTD &= 0x0; //turn off all led
+		PORTB &= 0x0;
+		PORTC &= 0x0;
+		
+		FlashPort(Flashreg);
+		
+		//mydelay(3906); //delay 1 second
+		_delay_ms(1000);
+		Delay_Time = 390; //100 ms
+		Win_time = 0;
+	}
+	/*
+	if(Flashreg == 0)
+	{
+		All_On(5);
+		Direction ^= 1;
+		if(Win_time > 1)
+		{
+			Delay_Time -= 2;
+			if(Delay_Time < 1)
+			{
+				Delay_Time = 10;
+				Win_time = 0;
+			}
+		}
+		Win_time++;
+	}
+	else
+	{
+		PORTD &= 0x0; //turn off all led
+		PORTB &= 0x0;
+		PORTC &= 0x0;
+		
+		FlashPort(Flashreg);
+		
+		mydelay(100);
+		Delay_Time = 10;
+		Win_time = 0;
+	}//*/
+	Flag = 1;
+}
 
 void Light_Direction()
 {
 	if (Direction)
 	{
 		Cycle_lights();
-	} 
+	}
 	else
 	{
 		Cycle_lights_Rev();
@@ -138,7 +212,7 @@ void All_On (int Flashtime)
 	int Count = 0;
 	while (Count < Flashtime)
 	{
-		PORTB &= 0x0; 
+		PORTB &= 0x0;
 		PORTD &= 0x08; //all off but PD3 the target led
 		PORTC &= 0x0;
 		_delay_ms(100);
@@ -147,6 +221,7 @@ void All_On (int Flashtime)
 		PORTD |= 0xEF;
 		PORTC |= 0x3C;
 		_delay_ms(100);
+		//mydelay(10); //100ms delay
 		
 		Count++;
 	}
@@ -159,46 +234,11 @@ void mydelay(volatile int Delay)
 	{
 		_delay_ms(10);
 	}*/
-	//TCNT1 = Delay;
 	OCR1A = Delay;
-}
-
-ISR(PCINT1_vect)
-{
-	if( (PINC & (1<<button)) == 0 ) //button goes low
-	{
-		if(Flashreg == 0)
-		{
-			All_On(5);
-			Direction ^= 1;
-			if(Win_time > 1)
-			{
-				Delay_Time -= 78; //subtract 20ms
-				if(Delay_Time < 1)
-				{
-					Delay_Time = 390; //reset to 100ms
-					Win_time = 0;
-				}
-			}
-			Win_time++;
-		}
-		else
-		{
-			PORTD &= 0x0; //turn off all led
-			PORTB &= 0x0;
-			PORTC &= 0x0;
-			
-			FlashPort(Flashreg);
-			
-			//mydelay(3906); //delay 1 second
-			_delay_ms(1000);	
-			Delay_Time = 390; //100 ms
-			Win_time = 0;
-		}
-	}
 }
 
 ISR(TIMER1_COMPA_vect)
 {
+	if (Flag)
 	Light_Direction();
 }
